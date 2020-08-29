a91cf70eb6e054b7d9038962d70e2dcfed70994b
zaryab2000 <zaryabafser2000@gmail.com>
2020-08-29 19:40:23 +0530

    added Vertical and Shoot Function

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,8 @@ class App extends Component{
       address: "",
       newName: "",
       horizontalShift: 0,
+      verticalShift: 0,
+      alienName: "",
       transaction_receipt: "",
     };
 
@@ -25,6 +27,10 @@ class App extends Component{
     this.handleNameChanger = this.handleNameChanger.bind(this);
     this.shiftHorizontal = this.shiftHorizontal.bind(this);
     this.handleHorizontalShift = this.handleHorizontalShift.bind(this);
+    this.shiftVertical = this.shiftVertical.bind(this);
+    this.handleVerticalShift = this.handleVerticalShift.bind(this);
+    this.startShoot = this.startShoot.bind(this);
+    this.handleShoot = this.handleShoot.bind(this);
   }
 
   handleNameChanger = (event) =>{
@@ -38,6 +44,20 @@ class App extends Component{
       horizontalShift : event.target.value,
     })
   }
+
+    handleVerticalShift = (event) =>{
+    this.setState({
+      verticalShift : event.target.value,
+    })
+  }
+
+    handleShoot = (event) =>{
+    this.setState({
+      alienName : event.target.value,
+    })
+  }
+
+
 
   async nameChanger(){
 
@@ -103,6 +123,69 @@ class App extends Component{
     );
   }
 
+    async shiftVertical(){
+
+    console.log(this.state.verticalShift)
+     var keystore = key,
+      amount = 1,
+      fee = 100000,
+      storage_limit = 1000,
+      gas_limit = 200000,
+      entry_point = undefined,
+      parameters = `(Right (Left ${this.state.horizontalShift}))`,
+      derivation_path = "";
+
+      const result = await TezosNodeWriter.sendContractInvocationOperation(
+      tezosNode,
+      keystore,
+      contractAddress,
+      amount,
+      fee,
+      derivation_path,
+      storage_limit,
+      gas_limit,
+      entry_point,
+      parameters,
+      TezosParameterFormat.Michelson
+    );
+
+    this.setState({ transaction_receipt:  result.operationGroupID});
+    alert(
+      `Injected operation ! \n Invocation Group ID : ${result.operationGroupID}`
+    );
+  }
+
+   async startShoot(){
+
+    console.log(this.state.alienName)
+     var keystore = key,
+      amount = 1,
+      fee = 100000,
+      storage_limit = 1000,
+      gas_limit = 200000,
+      entry_point = undefined,
+      parameters = `(Right (Right "${this.state.alienName}"))`,
+      derivation_path = "";
+
+      const result = await TezosNodeWriter.sendContractInvocationOperation(
+      tezosNode,
+      keystore,
+      contractAddress,
+      amount,
+      fee,
+      derivation_path,
+      storage_limit,
+      gas_limit,
+      entry_point,
+      parameters,
+      TezosParameterFormat.Michelson
+    );
+
+    this.setState({ transaction_receipt:  result.operationGroupID});
+    alert(
+      `Injected operation ! \n Invocation Group ID : ${result.operationGroupID}`
+    );
+  }
   render(){
      return (
     <div className="App">
@@ -182,6 +265,68 @@ class App extends Component{
           </div>
         </div>
 
+           <div class="row">
+          <div class="col-sm-6">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Change the NAME</h5>
+             <div className="input-group mb-2">
+              <div className="input-group-prepend">
+                <div className="input-group-text">Make Vertical Shift</div>
+              </div>
+              <input
+                type="text"
+                className="form-control"
+                id="nameChanger"
+                name="nameChanger"
+                placeholder="Enter the NAME"
+                value={this.state.verticalShift}
+                onChange={this.handleVerticalShift}
+              />
+            </div>
+
+          <div className="col-auto">
+            <button
+              type="submit"
+              className="btn btn-primary mb-2"
+              onClick={this.shiftVertical}
+            >Change
+            </button>
+          </div>
+                </div>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">ShOOT Alien</h5>
+                <div className="input-group mb-2">
+              <div className="input-group-prepend">
+                <div className="input-group-text">ENter Alien Name</div>
+              </div>
+              <input
+                type="text"
+                className="form-control"
+                id="horizontalShift"
+                name="horizontalShift"
+                placeholder="Enter X"
+                value={this.state.alienName}
+                onChange={this.handleShoot}
+              />
+            </div>
+              <div className="col-auto">
+            <button
+              type="submit"
+              className="btn btn-primary mb-2"
+              onClick={this.startShoot}
+            >Change
+            </button>
+          </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
         
       </div>
     </div>
